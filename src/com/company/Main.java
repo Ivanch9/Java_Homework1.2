67bad7c7dc023ff63c623c31b98626e651ab7dc3
Ivanch9 <sharkshaiba98@gmail.com>
2022-07-18 16:48:26 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int sum = 0;
+    int start = 1000;
+    int end = 0;
+    int step = 2;
+	for (int i = start; i > end; i -= step) {
+	   System.out.println(i);
+    }
+    }
+}
